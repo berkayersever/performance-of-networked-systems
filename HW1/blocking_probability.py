19425cbfa96ba638c4b74d75ef379b19e82ef941
Berkay Ersever <berkayersever@sabanciuniv.edu>
2021-11-24 23:51:25 +0300

    Updated blocking_probability.py

--- HW1/blocking_probability.py
+++ HW1/blocking_probability.py
@@ -1,5 +1,9 @@
 import math
-def blocking_probability():
-  print((1 / math.factorial(5)) / (sum(1 / math.factorial(i) for i in range(0, 6))))
 
-blocking_probability()
+
+def blocking_probability(n):
+    print("{}\tchannels blocking probability: {}".format(n, f'{(1 / math.factorial(n)) / (sum(1 / math.factorial(i) for i in range(0, n + 1))):.20f}'))
+
+
+blocking_probability(5)
+blocking_probability(10)
